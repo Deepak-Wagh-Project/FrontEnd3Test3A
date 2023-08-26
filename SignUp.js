d2e5218105e64d5a91a1cd6ca134ccff8c4751b6
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-08-26 19:49:38 +0530

    Update SignUp.js

--- SignUp.js
+++ SignUp.js
@@ -7,12 +7,9 @@ const confirmPasswordBox=document.getElementById("confirm-password-input")
 const error= document.createElement("div");
 error.id="error";
 error.innerHTML=`Error : All the fields are mandatory`;
-if(localStorage.getItem('token')===`null`||localStorage.getItem('token')===null){
- 
+if(localStorage.getItem('token').length>4){
+  window.location.href="Profile.html"
   
-}
-else{
-    window.location.href="Profile.html"
 }
 
 signUpButton.addEventListener("click",()=>{
